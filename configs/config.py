d5bd92ab1539888cd631640ddaffbd51aeba0825
zehua-wu <wuzehua15@163.com>
2024-12-09 18:44:28 -0500

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -1,7 +1,7 @@
 # config.py
 import os
 
-BASE_DIR = './data/Zdata'  # Path to your dataset directory
+BASE_DIR = './data/bdd100k'  # Path to your dataset directory
 
 CONFIG = {
     "train_images_dir": os.path.join(BASE_DIR, 'img_dir/train'),
@@ -9,7 +9,7 @@ CONFIG = {
     "val_images_dir": os.path.join(BASE_DIR, 'img_dir/val'),
     "val_annotations_dir": os.path.join(BASE_DIR, 'ann_dir/val'),
     "batch_size": 16,
-    "num_classes": 12,
+    "num_classes": 19,
     "learning_rate": 1e-3,
     "momentum": 0.9,
     "weight_decay": 5e-4,
@@ -18,12 +18,13 @@ CONFIG = {
     "resume_training": False,
     "visualize":True,
     "num_visualize_samples":5,
-    "visualize_output_dir": "./outputs/visualizations/run20241209"
+    "visualize_output_dir": "./outputs/visualizations"
 }
 
 
 CLASS_NAMES = [
-    "class0", "class1", "class2", "class3", "class4", "class5",
-    "class6", "class7", "class8", "class9", "class10", "class11",
-    
+    "class0: road", "class1: sidewalk", "class2: building", "class3: wall", "class4: fence", "class5: pole",
+    "class6: traffic light", "class7: traffic sign", "class8: vegetation", "class9: terrain", "class10: sky", "class11: person",
+    "class12: rider", "class13: car", "class14: truck", "class15: bus", "class16: train", "class17: motocycle", 
+    "class18: bicycle"
 ]
