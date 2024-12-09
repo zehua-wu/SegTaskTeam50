59da639ca4faaffd6ec6247d32ee93169aa43b97
zehua-wu <wuzehua15@163.com>
2024-12-08 20:43:59 -0500

    Update deeplabv3.py

--- models/deeplabv3.py
+++ models/deeplabv3.py
@@ -2,53 +2,24 @@ import torch.nn as nn
 import torchvision.models.segmentation
 
 
-class MyDeepLab(nn.Module):
-    def __init__(self, num_classes):
-        super(MyDeepLab, self).__init__()
-        resnet = torchvision.models.resnet50(pretrained=True)
+def create_deeplabv3(num_classes, pretrained=False):
+    """
+    Create a DeepLabv3 model and modify its classifier for the specified number of classes.
 
-        # Backbone with layers up to layer4
-        self.backbone = nn.Sequential(*list(resnet.children())[:-2])  # ResNet backbone
-        self.layer3 = nn.Sequential(*list(resnet.children())[:-3])   # Intermediate layer3
+    Args:
+        num_classes (int): Number of output classes.
+        pretrained (bool): Whether to load pretrained weights.
 
-        # ASPP Module
-        self.aspp = ASPP(in_channels=2048, out_channels=256)
+    Returns:
+        model (torch.nn.Module): Modified DeepLabv3 model.
+    """
+    model = torchvision.models.segmentation.deeplabv3_resnet50(pretrained=pretrained)
 
-        # Decoder module (for main output)
-        self.decoder = nn.Sequential(
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU()
-        )
+    # Modify the classifier for the specific number of classes
+    model.classifier[4] = nn.Conv2d(256, num_classes, kernel_size=(1, 1))
 
-        # Main classifier
-        self.classifier = nn.Conv2d(256, num_classes, kernel_size=1)
+    # Modify auxiliary classifier if it exists
+    if hasattr(model, "aux_classifier") and model.aux_classifier is not None:
+        model.aux_classifier[4] = nn.Conv2d(256, num_classes, kernel_size=(1, 1))
 
-        # Auxiliary classifier (on layer3 output)
-        self.aux_classifier = nn.Sequential(
-            nn.Conv2d(1024, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.Conv2d(256, num_classes, kernel_size=1)
-        )
-
-    def forward(self, x):
-        input_height, input_width = x.shape[2], x.shape[3]
-
-        # Backbone feature extractor
-        features = self.backbone(x)
-
-        # Auxiliary output from intermediate layer (layer3)
-        aux_features = self.layer3(x)  # Use features from layer3
-        aux_out = self.aux_classifier(aux_features)
-
-        # ASPP and Decoder
-        x = self.aspp(features)
-        x = self.decoder(x)
-        x = self.classifier(x)
-
-        # Upsample to input size
-        x = F.interpolate(x, size=(input_height, input_width), mode='bilinear', align_corners=True)
-        aux_out = F.interpolate(aux_out, size=(input_height, input_width), mode='bilinear', align_corners=True)
-
-        return OrderedDict({'out': x, 'aux': aux_out})
+    return model
